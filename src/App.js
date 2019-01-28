226932383d9ef613a5870719416397bb335ca7f5
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 19:01:25 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ export default class App extends Component{
   state = {
     spacePeeps: []
   }
-
+  
   componentDidMount(){
     fetch('http://api.open-notify.org/astros.json')
     .then(res => return res.json())
     .then(data =>
-    this.setState({
-      spacePeeps: data.people
-    }))
+      this.setState({
+        spacePeeps: data.people
+      })
+    )
   }
-
+  
   render(){
     return(<div>
       {this.state.spacePeeps.map(person=>person.name)}
