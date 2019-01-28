7eb565187476967cac373dd8595396fe199acb9c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 18:58:34 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import React, {Component} from 'react';
 
 export default class App extends Component{
 
+  state = {
+    spacePeeps:[]
+  }
   componentDidMount(){
     fetch('http://api.open-notify.org/astros.json')
     .then(data => return data.json())
+    .then(data =>
+    this.setState({
+      spacePeeps: data.people
+    }))
+  }
+  
+  render(){
+    return {}
   }
 }
