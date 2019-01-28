43260b6cdfa7e5f00e1e4673ee9abe5fade0172c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 18:53:10 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
 // create your App component here
+import React from 'React';
+
+export default class App extends React{
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+    .then(data => data.json())
+    .then(return json)
+  }
+}
